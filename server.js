7366052efdd9ed41c3ec734a4498d011ad3bed5e
siddh <sidd@example.com>
2026-01-29 14:54:23 +0530

    Fix MySQL connection for Render

--- server.js
+++ server.js
@@ -15,10 +15,11 @@ app.use("/images", express.static(path.join(__dirname, "../images")));
 
 /* -------------------- MYSQL CONNECTION -------------------- */
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    database: "bike_store"
+    database: process.env.MYSQL_DATABASE,
+    port: process.env.MYSQL_PORT || 3306
 });
 
 db.connect(err => {
